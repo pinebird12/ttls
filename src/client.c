ca42f6791521c353505a2e6721efacabb90afbb7
Ivy Hawks <pinebird12@alia.furrymachine.ai>
2026-07-29 15:39:21 -0700

    Refractored client code

--- src/client.c
+++ src/client.c
@@ -5,46 +5,73 @@
 #include <unistd.h>
 #include "elgamal.h"
 
+
+/* 
+ * Create at attach a socket to an ip address using a port
+ *
+ * @param server_addr Server ip address struct
+ * @param port Port number to connect to
+ * @return Socket number
+ */
+int makeSocket(struct in_addr server_addr, int port) {
+
+    int sockD = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in serv_addr;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8888);
+    serv_addr.sin_addr = server_addr;
+
+    // Make a connection
+    int connect_status = connect(sockD, (struct sockaddr*)&serv_addr,
+            sizeof(serv_addr));
+    if (connect_status == -1) {
+        errno = 111;
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    } else {
+        return sockD;
+    }
+}
+
+/*
+ * Sends a message encrypted accoding to my implimentation of
+ * the elgamal encryption protocol.
+ *
+ * @param message Message to be sent
+ * @param server_addr Server destination IP address
+ *
+ * TODO: refractor important part of code and seperate socket
+ * handling to add extansablility and closure of socket when
+ * full transmission is complete, like in server.c
+ */
 int sendEncrypted(int message, struct in_addr server_addr) {
-	unsigned long long int public = 49273;
-	unsigned long long int generator = 5;
-	unsigned long long int prime = 2 * 32771 + 1;
-	unsigned long long int* msg;
-	msg = iVencrypt(prime, generator, public, message);
+    unsigned long long int public = 49273;
+    unsigned long long int generator = 5;
+    unsigned long long int prime = 2 * 32771 + 1;
+    unsigned long long int* msg;
+    msg = iVencrypt(prime, generator, public, message);
 
-        // open a socket on which to send the message
-	int sockD = socket(AF_INET, SOCK_STREAM, 0);
-	msg[0] = htonl(msg[0]);
-	msg[1] = htonl(msg[1]);
+    // open a socket on which to send the message
+    msg[0] = htonl(msg[0]);
+    msg[1] = htonl(msg[1]);
 
-	struct sockaddr_in serv_addr;
+    struct sockaddr_in serv_addr;
 
-        // Set up the server address
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(8888);
-	serv_addr.sin_addr = server_addr;
+    int sockD = makeSocket(server_addr, 8888);
 
-        // Make a connection
-	int connect_status = connect(sockD, (struct sockaddr*)&serv_addr,
-			sizeof(serv_addr));
-
-        // validate connection status
-	if (connect_status == -1) {
-		printf("Error\n");
-		return 1;
-	} else {
-		send(sockD, msg, 2 *  sizeof(unsigned long long int), 0);
-		free(msg);
-	}
-        // FIXME: In final version should wait to close the socket
-	close(sockD);
-	return 0;
+    // send message
+    send(sockD, msg, 2 *  sizeof(unsigned long long int), 0);
+    free(msg);
+    close(sockD);
+    return 0;
 }
 
 int main(int argv, char **argc) {
-	int message = atoi(argc[1]);
-	struct in_addr server_addr;
-        inet_pton(AF_INET, argc[2], &server_addr);
-	int result = sendEncrypted(message, server_addr);
-	return result;
+    int message = atoi(argc[1]);
+    struct in_addr server_addr;
+    inet_pton(AF_INET, argc[2], &server_addr);
+    int result = sendEncrypted(message, server_addr);
+    return result;
 }
--- src/server.c
+++ src/server.c
@@ -5,52 +5,62 @@
 #include <unistd.h>
 #include "elgamal.h"
 
+/* 
+ * Listens in on port 8888 for a message encrypted using my
+ * implimentation of the elgamal protocal and then decrypts it,
+ * before clising the port (and marking opt as 1 so the port can
+ * be reused)
+ * TODO: refractor socket handling to a new function so the
+ * socket object can be left open and then closed when
+ * transmission is marked as complete, rather than after every
+ * message
+ */
 unsigned long long int awaitDecrypt() {
 
-	unsigned long long int private = 16809; // private key
-						// public is: 49273
-						// uses 5 as a generator
-	unsigned long long int prime = 2 * 32771 + 1;
+    unsigned long long int private = 16809; // private key
+                                            // public is: 49273
+                                            // uses 5 as a generator
+    unsigned long long int prime = 2 * 32771 + 1;
 
-	int servSock = socket(AF_INET, SOCK_STREAM, 0);
+    int servSock = socket(AF_INET, SOCK_STREAM, 0);
 
-        // Ensure that the socket will keep open for the server to reuse it
-        int opt = 1;
-        setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    // Ensure that the socket will keep open for the server
+    // to reuse it
+    int opt = 1;
+    setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-        // Setup input address
-	struct sockaddr_in serv_addr;
+    // Setup input address
+    struct sockaddr_in serv_addr;
 
-        // Setup ip address to listen on, so input ends up here
-	char char_addr[] = "127.0.0.1";
-	struct in_addr loop_adress;
-	inet_pton(AF_INET, char_addr, &loop_adress);
+    // Setup ip address to listen on, so input ends up here
+    char char_addr[] = "127.0.0.1";
+    struct in_addr loop_adress;
+    inet_pton(AF_INET, char_addr, &loop_adress);
 
-        // Bind socket to listen to the hostname ip address
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(8888);
-        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// serv_addr.sin_addr = loop_adress;
+    // Bind socket to listen to the hostname ip address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8888);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	bind(servSock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    bind(servSock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-	listen(servSock, 1);
+    listen(servSock, 1);
 
-	int clientSocket = accept(servSock, NULL, NULL);
+    int clientSocket = accept(servSock, NULL, NULL);
 
-	unsigned long long int msg[2];
-	recv(clientSocket, (char*)msg, 2 * sizeof(unsigned long long int), 0);
-        // Decrypt input message
-	unsigned long long int cypher[2] = {ntohl(msg[0]), htonl(msg[1])};
-	unsigned long long int out = decrypt(prime, private, cypher);
-	close(clientSocket);
-	close(servSock);
+    unsigned long long int msg[2];
+    recv(clientSocket, (char*)msg, 2 * sizeof(unsigned long long int), 0);
+    // Decrypt input message
+    unsigned long long int cypher[2] = {ntohl(msg[0]), htonl(msg[1])};
+    unsigned long long int out = decrypt(prime, private, cypher);
+    close(clientSocket);
+    close(servSock);
 
-	return out;
+    return out;
 }
 
 int main(int argv, char **argc) {
-	unsigned long long int message = awaitDecrypt();
-	printf("Message out: %lld\n", message);
-	return 0;
+    unsigned long long int message = awaitDecrypt();
+    printf("Message out: %lld\n", message);
+    return 0;
 } 
